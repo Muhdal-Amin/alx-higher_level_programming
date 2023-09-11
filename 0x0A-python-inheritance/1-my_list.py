eb6960ca6b16be8384a26e29c9637ec5c2e52478
Muhdal-Amin <adalemo2000@gmail.com>
2023-09-11 22:22:58 +0000

    File - Added

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Defines MyList function"""
+
+class MyList(list):
+    """The __init__ method initialize"""
+    def __init__(self):
+        """empty class"""
+        pass
+
+    def print_sorted(self):
+        """inherits from list"""
+        print(sorted(self))
