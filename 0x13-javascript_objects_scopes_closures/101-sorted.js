3524411e3f333ba1669b3abf2a437216ca8cde81
Muhdal-Amin <adalemo2000@gmail.com>
2023-11-14 22:12:37 +0000

    Semistandard checks passed

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -2,10 +2,10 @@
 const dict = require('./101-data').dict;
 const newDict = {};
 for (const userId in dict) {
-    const occurrences = dict[userId];
-    if (!newDict[occurrences]) {
-	newDict[occurrences] = [];
-    }
-    newDict[occurrences].push(userId);
+  const occurrences = dict[userId];
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
+  }
+  newDict[occurrences].push(userId);
 }
 console.log(newDict);
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let counter = 0;
-    for (const element of list) {
-	if (element === searchElement) {
-	    counter++;
-	}
+  let counter = 0;
+  for (const element of list) {
+    if (element === searchElement) {
+      counter++;
     }
-    return counter;
+  }
+  return counter;
 };
