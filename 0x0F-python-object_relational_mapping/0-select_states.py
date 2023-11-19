2605d461391c5a4fbd08ee70fe43b67c284212f0
Muhdal-Amin <adalemo2000@gmail.com>
2023-11-19 14:48:40 +0000

    A script that lists all states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 """A script that lists all states from the database hbtn_0e_usa"""
-from sys import argv
 import MySQLdb
+from sys import argv
 
 
 if __name__ == "__main__":
-    # script arguments
     mysql_username = argv[1]
     mysql_password = argv[2]
     database_name = argv[3]
