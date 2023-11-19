4b7fbbce70fdbe6cd52d34e110fd85909f95ab22
Muhdal-Amin <adalemo2000@gmail.com>
2023-11-19 15:16:11 +0000

    A script that takes in an argument and displays all values in the states table where name matches the aegument

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,19 +12,20 @@ if __name__ == "__main__":
 
     db = MySQLdb.connect(
         host='localhost',
+        port=3306,
         user=username,
         passwd=password,
         db=database
     )
 
-    cursor = db.cursor()
-    cursor.execute("""SELECT * FROM states WHERE name LIKE BINARY\
-    '{}' ORDER BY id ASC""".format(searched))
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states WHERE name LIKE BINARY\
+    '{}' ORDER BY id ASC""".format(state_name_searched))
 
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
 
     for row in rows:
         print(row)
 
-    cursor.close()
+    cur.close()
     db.close()
