bfd774e488060d3c3f60f488662f555c17226481
Muhdal-Amin <adalemo2000@gmail.com>
2023-11-19 22:33:01 +0000

    A script that selects states from the database

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,24 @@
 #!/usr/bin/python3
-"""A script that lists all states from the databse"""
+"""A script that selects states from the database"""
 import MySQLdb
-from sys import argv
+import sys
+
 
 if __name__ == "__main__":
-    username = argv[1]
-    password = argv[2]
-    database = argv[3]
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=username,
-        passwd=password,
-        db=database
-    )
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database
+                         )
     cur = db.cursor()
     cur.execute('SELECT * FROM states ORDER BY id ASC')
     rows = cur.fetchall()
     for row in rows:
         print(row)
-
     cur.close()
     db.close()
