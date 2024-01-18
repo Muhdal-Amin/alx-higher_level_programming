95196d18a1bc36b26015204ccdf7da5b09c117cb
Muhdal-Amin <adalemo2000@gmail.com>
2024-01-18 21:58:25 +0000

    A script that lists all shows contained in the database hbtn_0d_tvshows

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- A script that lists all shows contained in the database hbtn_0d_tvshows
+-- Records are sorted in ascending orderby tv_shows.title and tv_show_genres.genre_id
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
