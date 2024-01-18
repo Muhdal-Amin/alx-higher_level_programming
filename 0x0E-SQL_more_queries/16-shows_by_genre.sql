04dbb9f0507225227d110de25d4042ac73b7cbae
Muhdal-Amin <adalemo2000@gmail.com>
2024-01-18 22:11:36 +0000

    A script that lists all shows and genres linked to that show

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,10 @@
+-- A script that lists all shows and genres linked to that show
+-- Results are sorted in ascending order by the show title and genre name
+SELECT t.`title`, g.`name`
+  FROM `tv_shows` AS t
+       LEFT JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       LEFT JOIN `tv_genres` AS g
+       ON s.`genre_id` = g.`id`
+ ORDER BY t.`title`, g.`name`;
